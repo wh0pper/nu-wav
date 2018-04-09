58efa68ca1556ddd11a898ce8fb5500aa2875b81
Kaitlyn <katiefujihara@gmail.com>
2018-04-09 16:24:04 -0700

    add styling

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { Vis1Component } from './vis1/vis1.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    Vis1Component
+    Vis1Component,
+    WelcomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule

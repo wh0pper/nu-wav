3c178e2a08c4b1523a24149f73e5088812d28e33
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-04-09 15:36:34 -0700

    add soundcloud api component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { Vis1Component } from './vis1/vis1.component';
+import { SpotifyComponent } from './spotify/spotify.component';
+import { SoundcloudComponent } from './soundcloud/soundcloud.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    Vis1Component
+    Vis1Component,
+    SpotifyComponent,
+    SoundcloudComponent
   ],
   imports: [
     BrowserModule

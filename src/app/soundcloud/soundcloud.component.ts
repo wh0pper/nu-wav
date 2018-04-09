565118f02693c447c73f6e2ecf58ff002fe5174f
Jared Clemmensen <jaredclemmensen@gmail.com>
2018-04-09 16:38:19 -0700

    play hardcoded song with soundcloud

--- src/app/soundcloud/soundcloud.component.ts
+++ src/app/soundcloud/soundcloud.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as SC from 'soundcloud';
+import { soundcloud_key } from '../../api-keys';
 
 @Component({
   selector: 'app-soundcloud',
@@ -6,10 +8,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./soundcloud.component.css']
 })
 export class SoundcloudComponent implements OnInit {
-
+  public tracks;
   constructor() { }
 
   ngOnInit() {
+    console.log('sc initialize');
+    SC.initialize({
+
+      client_id: soundcloud_key,
+      //redirect_uri: 'http://example.com/callback'
+    });
+    //
   }
 
+  playSong() {
+    let songURL = "https://soundcloud.com/mad-colour/die-antwoord-dis-is-why-im-hot-madcolour-remix";
+
+    // SC.get('/resolve.json?url=' + songURL).then(function(tracks){
+    //   console.log(tracks.id)
+    //   SC.stream('/tracks/' + tracks.id).then(function(player){
+    //     player.play();
+    //   });
+    });
+  }
 }
